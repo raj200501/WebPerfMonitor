ad641261966682ea403741790c1bb492e9ac6eae
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-07-13 00:55:02 -0700

    Create deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Set up necessary configurations for deployment
+
+# Deploy the application (this script can be extended based on the specific deployment requirements)
+echo "Deploying WebPerfMonitor application..."
